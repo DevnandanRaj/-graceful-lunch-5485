4b376385a18ab13f734bc6d81f220f2fdfeb84ce
DevnandanRaj <devnandanraj07@gmail.com>
2023-01-21 23:23:05 +0530

    added day-5

--- Industry_Clone/home.js
+++ Industry_Clone/home.js
@@ -27,3 +27,71 @@ window.addEventListener("load", function () {
 
 
 //products api
+let API = "https://63c66b68d307b7696738a8cf.mockapi.io/products"
+    async function FetchData(){
+      try{
+        let request=await fetch(API);
+       let  data= await request.json();
+        console.log(data);
+        DisplayProduct(data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    FetchData()
+
+    let CartArr = JSON.parse(localStorage.getItem("cart"))||[];
+
+    let Container = document.getElementById("product-container")
+
+    function DisplayProduct(data){
+      
+
+      Container.innerHTML = "";
+      data.forEach((product,i) => {
+        let card = document.createElement("div");
+        let image = document.createElement("img");
+        let brand = document.createElement("h3");
+        let category = document.createElement("p");
+        let details = document.createElement("p");
+        let price = document.createElement("h4");
+        let add_to_cart = document.createElement("button");
+
+       
+        image.src = product.image;
+        brand.textContent = product.name;
+        category.textContent = product.category;
+        price.textContent = `₹${product.price}`;
+        details.textContent = product.title;
+        add_to_cart.textContent = "Add to Cart";
+
+        add_to_cart.addEventListener("click",()=>{
+            if(checkDuplicate(product)){
+              alert("Product Already in Cart")
+            }else{
+              CartArr.push({...product,quantity:1});
+              localStorage.setItem("cart",JSON.stringify(CartArr));
+              alert("Product Added To Cart")
+            }
+        })
+
+
+        card.append(image,brand,price,details,category,add_to_cart);
+        Container.append(card);
+
+      });
+    
+ 
+
+    }
+   
+   function checkDuplicate(product){
+    for(let i=0; i<CartArr.length;i++){
+
+      if(CartArr[i].id===product.id){
+        return true;
+      
+      }
+    }
+    return false;
+    }
